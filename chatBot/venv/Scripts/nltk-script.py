3af7c117881a874449f7b5ed63515f93a5c202f7
Tawishi <tawishisharma1@gmail.com>
2021-01-17 21:18:34 +0530

    Added financial hack project

---
+++ chatBot/venv/Scripts/nltk-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\Dell\OneDrive\Desktop\2019-2023\LHD Build\chatBot\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'nltk==3.5','console_scripts','nltk'
+__requires__ = 'nltk==3.5'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('nltk==3.5', 'console_scripts', 'nltk')()
+    )
