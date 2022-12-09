67ed90de56cb57210d1001d2e6b894718b566b5d
KimSeongjin <darkmoon625@naver.com>
2022-12-09 20:08:11 +0900

    merge

--- SimpleChess_GamePYTHON/chess game simple/gui.py
+++ SimpleChess_GamePYTHON/chess game simple/gui.py
@@ -1,5 +1,5 @@
 import pygame
-import chess
+import chessgame
 import sys
 from random import choice
 from traceback import format_exc
@@ -78,92 +78,92 @@ def print_empty_board():
 
 
 def paint_square(square, square_color):
-    col = chess.FILES.index(square[0])
-    row = 7-chess.RANKS.index(square[1])
+    col = chessgame.FILES.index(square[0])
+    row = 7-chessgame.RANKS.index(square[1])
     pygame.draw.rect(SCREEN, square_color, (SQUARE_SIDE*col,
                      SQUARE_SIDE*row, SQUARE_SIDE, SQUARE_SIDE), 0)
 
 
 def paint_dark_squares(square_color):
-    for position in chess.single_gen(chess.DARK_SQUARES):
-        paint_square(chess.bb2str(position), square_color)
+    for position in chessgame.single_gen(chessgame.DARK_SQUARES):
+        paint_square(chessgame.bb2str(position), square_color)
 
 
 def get_square_rect(square):
-    col = chess.FILES.index(square[0])
-    row = 7-chess.RANKS.index(square[1])
+    col = chessgame.FILES.index(square[0])
+    row = 7-chessgame.RANKS.index(square[1])
     return pygame.Rect((col*SQUARE_SIDE, row*SQUARE_SIDE), (SQUARE_SIDE, SQUARE_SIDE))
 
 
-def coord2str(position, color=chess.WHITE):
-    if color == chess.WHITE:
+def coord2str(position, color=chessgame.WHITE):
+    if color == chessgame.WHITE:
         file_index = int(position[0]/SQUARE_SIDE)
         rank_index = 7 - int(position[1]/SQUARE_SIDE)
-        return chess.FILES[file_index] + chess.RANKS[rank_index]
-    if color == chess.BLACK:
+        return chessgame.FILES[file_index] + chessgame.RANKS[rank_index]
+    if color == chessgame.BLACK:
         file_index = 7 - int(position[0]/SQUARE_SIDE)
         rank_index = int(position[1]/SQUARE_SIDE)
-        return chess.FILES[file_index] + chess.RANKS[rank_index]
+        return chessgame.FILES[file_index] + chessgame.RANKS[rank_index]
 
 
-def print_board(board, color=chess.WHITE):
-    if color == chess.WHITE:
+def print_board(board, color=chessgame.WHITE):
+    if color == chessgame.WHITE:
         printed_board = board
-    if color == chess.BLACK:
-        printed_board = chess.rotate_board(board)
+    if color == chessgame.BLACK:
+        printed_board = chessgame.rotate_board(board)
 
     print_empty_board()
 
-    if chess.is_check(board, chess.WHITE):
-        paint_square(chess.bb2str(chess.get_king(
-            printed_board, chess.WHITE)), RED_CHECK)
-    if chess.is_check(board, chess.BLACK):
-        paint_square(chess.bb2str(chess.get_king(
-            printed_board, chess.BLACK)), RED_CHECK)
+    if chessgame.is_check(board, chessgame.WHITE):
+        paint_square(chessgame.bb2str(chessgame.get_king(
+            printed_board, chessgame.WHITE)), RED_CHECK)
+    if chessgame.is_check(board, chessgame.BLACK):
+        paint_square(chessgame.bb2str(chessgame.get_king(
+            printed_board, chessgame.BLACK)), RED_CHECK)
 
-    for position in chess.colored_piece_gen(printed_board, chess.KING, chess.BLACK):
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.KING, chessgame.BLACK):
         SCREEN.blit(pygame.transform.scale(BLACK_KING,   (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
-    for position in chess.colored_piece_gen(printed_board, chess.QUEEN, chess.BLACK):
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.QUEEN, chessgame.BLACK):
         SCREEN.blit(pygame.transform.scale(BLACK_QUEEN,  (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
-    for position in chess.colored_piece_gen(printed_board, chess.ROOK, chess.BLACK):
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.ROOK, chessgame.BLACK):
         SCREEN.blit(pygame.transform.scale(BLACK_ROOK,   (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
-    for position in chess.colored_piece_gen(printed_board, chess.BISHOP, chess.BLACK):
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.BISHOP, chessgame.BLACK):
         SCREEN.blit(pygame.transform.scale(BLACK_BISHOP, (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
-    for position in chess.colored_piece_gen(printed_board, chess.KNIGHT, chess.BLACK):
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.KNIGHT, chessgame.BLACK):
         SCREEN.blit(pygame.transform.scale(BLACK_KNIGHT, (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
-    for position in chess.colored_piece_gen(printed_board, chess.PAWN, chess.BLACK):
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.PAWN, chessgame.BLACK):
         SCREEN.blit(pygame.transform.scale(BLACK_PAWN,   (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
-    for position in chess.colored_piece_gen(printed_board, chess.JOKER, chess.BLACK):
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.JOKER, chessgame.BLACK):
         SCREEN.blit(pygame.transform.scale(BLACK_JOKER,  (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
 
-    for position in chess.colored_piece_gen(printed_board, chess.KING, chess.WHITE):
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.KING, chessgame.WHITE):
         SCREEN.blit(pygame.transform.scale(WHITE_KING,   (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
-    for position in chess.colored_piece_gen(printed_board, chess.QUEEN, chess.WHITE):
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.QUEEN, chessgame.WHITE):
         SCREEN.blit(pygame.transform.scale(WHITE_QUEEN,  (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
-    for position in chess.colored_piece_gen(printed_board, chess.ROOK, chess.WHITE):
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.ROOK, chessgame.WHITE):
         SCREEN.blit(pygame.transform.scale(WHITE_ROOK,   (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
-    for position in chess.colored_piece_gen(printed_board, chess.BISHOP, chess.WHITE):
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.BISHOP, chessgame.WHITE):
         SCREEN.blit(pygame.transform.scale(WHITE_BISHOP, (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
-    for position in chess.colored_piece_gen(printed_board, chess.KNIGHT, chess.WHITE):
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.KNIGHT, chessgame.WHITE):
         SCREEN.blit(pygame.transform.scale(WHITE_KNIGHT, (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
-    for position in chess.colored_piece_gen(printed_board, chess.PAWN, chess.WHITE):
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.PAWN, chessgame.WHITE):
         SCREEN.blit(pygame.transform.scale(WHITE_PAWN,   (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
-    for position in chess.colored_piece_gen(printed_board, chess.JOKER, chess.WHITE):
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
+    for position in chessgame.colored_piece_gen(printed_board, chessgame.JOKER, chessgame.WHITE):
         SCREEN.blit(pygame.transform.scale(WHITE_JOKER,  (SQUARE_SIDE,
-                    SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
+                    SQUARE_SIDE)), get_square_rect(chessgame.bb2str(position)))
 
     pygame.display.flip()
 
@@ -175,16 +175,16 @@ def set_title(title):
 
 def make_AI_move(game, color):
     set_title(SCREEN_TITLE + ' - Calculating move...')
-    new_game = chess.make_move(game, chess.get_AI_move(game, AI_SEARCH_DEPTH))
+    new_game = chessgame.make_move(game, chessgame.get_AI_move(game, AI_SEARCH_DEPTH))
     set_title(SCREEN_TITLE)
     print_board(new_game.board, color)
     return new_game
 
 
 def try_move(game, attempted_move):
-    for move in chess.legal_moves(game, game.to_move):
+    for move in chessgame.legal_moves(game, game.to_move):
         if move == attempted_move:
-            game = chess.make_move(game, move)
+            game = chessgame.make_move(game, move)
     return game
 
 
@@ -198,15 +198,15 @@ def play_as(game, color):
             CLOCK.tick(CLOCK_TICK)
             print_board(game.board, color)
 
-            if chess.game_ended(game):
-                set_title(SCREEN_TITLE + ' - ' + chess.get_outcome(game))
+            if chessgame.game_ended(game):
+                set_title(SCREEN_TITLE + ' - ' + chessgame.get_outcome(game))
                 ongoing = False
 
-            if ongoing and game.to_move == chess.opposing_color(color):
+            if ongoing and game.to_move == chessgame.opposing_color(color):
                 game = make_AI_move(game, color)
 
-            if chess.game_ended(game):
-                set_title(SCREEN_TITLE + ' - ' + chess.get_outcome(game))
+            if chessgame.game_ended(game):
+                set_title(SCREEN_TITLE + ' - ' + chessgame.get_outcome(game))
                 ongoing = False
 
             for event in pygame.event.get():
@@ -220,8 +220,8 @@ def play_as(game, color):
                     arriving_square = coord2str(event.pos, color)
 
                     if ongoing and game.to_move == color:
-                        move = (chess.str2bb(leaving_square),
-                                chess.str2bb(arriving_square))
+                        move = (chessgame.str2bb(leaving_square),
+                                chessgame.str2bb(arriving_square))
                         game = try_move(game, move)
                         print_board(game.board, color)
 
@@ -231,8 +231,8 @@ def play_as(game, color):
                     if event.key == 104 and ongoing:  # H key
                         game = make_AI_move(game, color)
                     if event.key == 117:  # U key
-                        game = chess.unmake_move(game)
-                        game = chess.unmake_move(game)
+                        game = chessgame.unmake_move(game)
+                        game = chessgame.unmake_move(game)
                         set_title(SCREEN_TITLE)
                         print_board(game.board, color)
                         ongoing = True
@@ -246,13 +246,13 @@ def play_as(game, color):
                         print(game.get_move_list() + '\n')
                         print('\n'.join(game.position_history))
                     if event.key == 101:  # E key
-                        print('eval = ' + str(chess.evaluate_game(game)/100))
+                        print('eval = ' + str(chessgame.evaluate_game(game)/100))
                     if event.key == 106:  # J key
                         joker += 1
-                        if joker == 13 and chess.get_queen(game.board, color):
-                            queen_index = chess.bb2index(
-                                chess.get_queen(game.board, color))
-                            game.board[queen_index] = color | chess.JOKER
+                        if joker == 13 and chessgame.get_queen(game.board, color):
+                            queen_index = chessgame.bb2index(
+                                chessgame.get_queen(game.board, color))
+                            game.board[queen_index] = color | chessgame.JOKER
                             print_board(game.board, color)
 
                 if event.type == pygame.VIDEORESIZE:
@@ -267,23 +267,23 @@ def play_as(game, color):
         bug_file.write('----- ' + strftime('%x %X') + ' -----\n')
         bug_file.write(format_exc())
         bug_file.write('\nPlaying as WHITE:\n\t' if color ==
-                       chess.WHITE else '\nPlaying as BLACK:\n\t')
+                       chessgame.WHITE else '\nPlaying as BLACK:\n\t')
         bug_file.write(game.get_move_list() + '\n\t')
         bug_file.write('\n\t'.join(game.position_history))
         bug_file.write('\n-----------------------------\n\n')
         bug_file.close()
 
 
-def play_as_white(game=chess.Game()):
-    return play_as(game, chess.WHITE)
+def play_as_white(game=chessgame.Game()):
+    return play_as(game, chessgame.WHITE)
 
 
-def play_as_black(game=chess.Game()):
-    return play_as(game, chess.BLACK)
+def play_as_black(game=chessgame.Game()):
+    return play_as(game, chessgame.BLACK)
 
 
-def play_random_color(game=chess.Game()):
-    color = choice([chess.WHITE, chess.BLACK])
+def play_random_color(game=chessgame.Game()):
+    color = choice([chessgame.WHITE, chessgame.BLACK])
     play_as(game, color)
 
 # play_as_white()
@@ -337,14 +337,14 @@ def blitz():
         SCREEN.blit(BLITZ_TEXT, BLITZ_RECT)
 
         TEXT = get_font(40).render(
-            "Blitz chess simply refers to a game of", True, "white")
+            "Blitz chessgame simply refers to a game of", True, "white")
         TEXT_RECT = TEXT.get_rect()
         TEXT_RECT.centerx = round(400)
         TEXT_RECT.y = 220
         SCREEN.blit(TEXT, TEXT_RECT)
 
         TEXT = get_font(40).render(
-            "chess that has a fast time control.", True, "white")
+            "chessgame that has a fast time control.", True, "white")
         TEXT_RECT = TEXT.get_rect()
         TEXT_RECT.centerx = round(400)
         TEXT_RECT.y = 260
